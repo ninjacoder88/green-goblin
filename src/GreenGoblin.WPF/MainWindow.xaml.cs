a87de014e5ca1d999dbec8c7e716d2b78f186a28
tomharris <thomasharris88@gmail.com>
2017-04-02 20:24:26 -0500

    added unity nuget package; added additional buttons

--- src/GreenGoblin.WPF/MainWindow.xaml.cs
+++ src/GreenGoblin.WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Practices.Unity;
 
 namespace GreenGoblin.WPF
 {
@@ -23,6 +24,12 @@ namespace GreenGoblin.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [Dependency]
+        public GreenGoblinViewModel ViewModel
+        {
+            set { DataContext = value; }
         }
     }
 }
